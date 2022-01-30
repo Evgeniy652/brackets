1585e36fd2b5a8be6321691680dd6ceba18ac7ac
Evgeniy652 <evgen652@mail.ru>
2022-01-30 20:01:55 +0300

    feat:add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+	// your solution
+	const result = recursionOfFinding(str, bracketsConfig);
+  
+	return result.length === 0;
+  }
+  
+  function recursionOfFinding(originStr, bracketsConfig) {
+	let str = originStr;
+  
+	bracketsConfig.forEach(element => {
+	  const fullBracketsStr = element.join('');
+  
+	  str = recursionOfReplace(str, fullBracketsStr);
+	});
+  
+	if (originStr !== str && str.length !== 0) {
+	  return recursionOfFinding(str, bracketsConfig);
+	} else {
+	  return str;
+	}
+  }
+  
+  function recursionOfReplace(str, full) {
+	if (str.includes(full)) {
+	  str = str.replace(full, '');
+	  return recursionOfReplace(str, full);
+	} 
+  
+	return str;
+  }
